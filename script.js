b60f945b572bcf297a169c3b52204e1ba0e450b1
yetemegn-telaye <75949338+yetemegn-telaye@users.noreply.github.com>
2022-09-13 16:34:24 +0300

    Add popup for project description

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const overlay = document.querySelector('.mobile-overlay');
+const projectBtn = document.querySelectorAll('.project-detail');
+const projectPopup = document.querySelector('#project');
+const closeBtn = document.querySelector('.close');
 
 function mytoggleMenu() {
   overlay.classList.toggle('hide-js');
 }
 mytoggleMenu();
+
+projectBtn.forEach(project=>{
+  project.addEventListener('click', function(){
+    projectPopup.style.display="inline-block";
+  })
+});
+
+closeBtn.addEventListener('click', function(){
+  projectPopup.style.display="none";
+});
